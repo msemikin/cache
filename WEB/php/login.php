f4dd240d8ba0d96ab3e6fb465c7e9dbcf8df949c
Max Selekh <max.selekh@gmail.com>
2015-02-12 13:42:32 +0200

    можно зайти, как  учитель

--- WEB/php/login.php
+++ WEB/php/login.php
@@ -1,11 +1,19 @@
 <?php
 sleep(1);
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-	if ($_POST['username'] == 'admin' && $_POST['password'] == 'admin')
-		echo 1;
-	else
-		echo 0;
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['usertype'])) {
+    if($_POST['usertype'] == 'student'){
+        if ($_POST['username'] == 'admin' && $_POST['password'] == 'admin')
+            echo 1;
+        else
+            echo 0;
+    }
+    else if ($_POST['usertype'] == 'teacher'){
+        if ($_POST['username'] == 'admin' && $_POST['password'] == 'admin')
+            echo 2;
+        else
+            echo 0;
+    }
 } else {
 	echo 0;
 }
