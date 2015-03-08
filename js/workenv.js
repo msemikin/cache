cebe968a72e45e2489cd86c6083981bd7327092a
Ira <Ira@Ira-pc>
2015-03-08 13:18:24 +0200

    brr

--- js/workenv.js
+++ js/workenv.js
@@ -1,8 +1,17 @@
-var tabs = document.getElementsByName("tab");
-var contentDivs = document.getElementsByName("contentDiv");
+var field;
+var tabs;
+var contentDivs;
+var textObj;
+var canvas;
 
 window.onload = function() {
+    contentDivs = document.getElementsByName("contentDiv");
+    tabs = document.getElementsByName("tab");
+    canvas = document.getElementById("pages-container");
+    field = document.getElementById('file-field');
+    var chars = new Array (',', '.', ';', '!', ':', ' ');
 
+<<<<<<< HEAD
     var sample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     /*if(document.getElementsById('obj').checked = 'true')
@@ -18,7 +27,13 @@ window.onload = function() {
 
 
     });
+=======
+    var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+    $('#pages-container').text(text);
+>>>>>>> 1d50c157de4259a8f6e481a35bc1f5c1c3a02511
+
+    //
     for (i = 0; i < tabs.length; i++){
         tabs[i].onclick = changeTab;
         var bordWhite = document.createElement('div');
@@ -30,7 +45,8 @@ window.onload = function() {
         }
         tabs[i].appendChild(bordWhite);
     }
-    function getSelectionText() {
+    //работа Семикина
+    /*function getSelectionText() {
         var text = "";
         if (window.getSelection) {
             text = window.getSelection().toString();
@@ -60,28 +76,121 @@ window.onload = function() {
                 html = document.selection.createRange().htmlText;
             }
         }
-        alert(html);
-    }
-    var field = document.getElementById('file-field');
-    field.onchange = function (e) {
-        jDoc.read(e.target.files[0], {
-            success:
-            function (parsedFile) {
-                var canvas = document.getElementById("pages-container");
-                canvas.innerHTML = "";
-                canvas.appendChild(parsedFile.html());
-                var textLikeObject = parsedFile.data();
-                console.log(textLikeObject);
-            },
+        var intersection = FindIntersection(html, $('#pages-container').text());
+        if(intersection) {
+            html = $('#pages-container').text().slice(intersection.position,intersection.position + intersection.length);
+        }
+        if(html) {
+            window.alert(html)
+        }
+    }*/
 
-            error:
-            function (error) {
-                console.log(error);
+    //Чтение документа
+
+    var jD = new jDoc();
+
+    jD.on('readstart', function () {
+        canvas.innerHTML = "";
+        canvas.style.backgroundColor = "#fff";
+        console.log("START ", arguments);
+    });
+
+    jD.on('readend', function () {
+        console.log("END ", arguments);
+    });
+
+    jD.on('read', function (fileData) {
+        console.log("READ ", arguments);
+        console.log("File name -", fileData.getName());
+        console.log("Words count -", fileData.getWordsCount());
+        console.log("Pages count -", fileData.getPagesCount());
+
+        //Отображение html-файла
+        canvas.appendChild(fileData.html());
+
+        //ненужная фигня
+        /*var textObj = fileData.data();
+        console.log(textObj);
+        //тестовая роспись дока без стилей
+        for(i = 0; i < textObj.pages.length; i++)
+        {
+            for(j = 0; j < textObj.pages[i].children.length; j++)
+            {
+                for(k = 0; k < textObj.pages[i].children[j].children.length; k++)
+                {
+                    var temp = document.createElement('span');
+                    /*try
+                    {
+                        var tempin = textObj.pages[i].children[j].children[k].properties.textContent;
+                    }
+                    finally
+                    {
+                        continue;
+                    }
+                    var tempin = textObj.pages[i].children[j].children[k].properties.textContent;
+
+                    //проверка на пустой блок
+                    if (isSpases(tempin)) continue;
+
+
+                    var str = "";
+                    //если блок начинается
+                    if(!find(chars ,tempin[0]) && k!=0) str = " ";
+                    if(tempin[tempin.length-1] == ' ') tempin = tempin.substring(0, tempin.length - 1);
+                    str += tempin + '1';
+                    temp.innerHTML = str;
+                    canvas.appendChild(temp);
+                    if(k == textObj.pages[i].children[j].children.length - 1)
+                    {
+                        canvas.appendChild(document.createElement("br"));
+                        canvas.appendChild(document.createElement("br"));
+                    }
+
+                }
             }
-        });
+        }
+        console.log(canvas);*/
+
+         Array.prototype.forEach.call(document.querySelectorAll('.pages-container > div'), function (page) {
+            if (page.scrollHeight > page.offsetHeight) {
+                console.log('Invalid page', {
+                    page: page,
+                    pageHeight: page.offsetHeight,
+                    contentHeight: page.scrollHeight
+                });
+            }
+        })
+    });
+
+    jD.on('error', function () {
+        console.log("ERROR ", arguments);
+    });
+
+    field.onchange = function (e) {
+        jD.read(e.target.files[0]);
     };
 }
 
+function find (arr, char)
+{
+    for (c = 0; c < arr.length; c++)
+    {
+        if(char == arr[c]) return true;
+    }
+    return false;
+}
+
+function isSpases (array)
+{
+    for(v = 0; v < array.length; v++)
+    {
+        if(array[v] != ' ') return false;
+    }
+    return true;
+}
+
+
+//Переключение вкладок
 function changeTab() {
     for(j = 0; j < tabs.length; j++){
         if(this == tabs[j])
@@ -110,33 +219,19 @@ function changeTab() {
 }
 
 
-function initiateTextBox(text, wordCallback){
-    var className = 'word';
-    var processed = processText(text,className);
-    $('#pages-container').html(processed);
-
-    $('.word').dblclick(
-        function(){
-            wordCallback($(this).text());
+//Работа Семикина
+/*function FindIntersectionFromStart(a,b){
+    for(var i=a.length;i>0;i--){
+        d = a.substring(0,i);
+        j = b.indexOf(d);
+        if (j>=0){
+            return ({position:j,length:i});
         }
-    );
+    }
+
+    return null;
 }
-
-function processText(text, className){
-    
-    var dividers = ' ';
-    var words = text.split(dividers);
-
-    for (var i = 0; i < words.length; i++) {
-        words[i] = wrapToTag(words[i], 'span', className);
-    };
-
-    return words.join(' ');
-}
-
-function wrapToTag(string, tag, className){
-    return '<'+tag+' class="'+className+'"'+'>'+string+'</'+tag+'>';
-}
+<<<<<<< HEAD
 function disabled(field) {
  for (i=0; i<field.length; i++) { field[i].checked=false; }
 }
@@ -146,3 +241,24 @@ function disabled(field) {
 
 
 
+=======
+
+function FindIntersection(a,b){
+    var bestResult = null;
+    for(var i=0;i<a.length-1;i++){
+        var result = FindIntersectionFromStart(a.substring(i),b);
+        if (result){
+            if (!bestResult){
+                bestResult = result;
+            } else {
+                if (result.length>bestResult.length){
+                    bestResult = result;
+                }
+            }
+        }
+        if(bestResult && bestResult.length>=a.length-i)
+            break;
+    }
+    return bestResult;
+}*/
+>>>>>>> 1d50c157de4259a8f6e481a35bc1f5c1c3a02511
