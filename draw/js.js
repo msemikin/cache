17c162f359a85fe81e5e1cc43bf52ecb7a1bb77c
Maxim <caliber1997@gmail.com>
2015-03-28 23:47:50 +0200

    not even alpha draw

--- draw/js.js
+++ draw/js.js
@@ -1,34 +1,96 @@
-var figures = [];
-
-var start = function(){
-    this.data("oldt", this.transform().string);
-};
-var move = function(dx,dy){
-    var values = this.data("oldt").split(/[t,]/).filter( function(el, index, arr){
-        return el ? true : false;
-    });
-    oldx = parseInt(values[0] || 0);
-    oldy = parseInt(values[1] || 0);
-    var newDx = dx + oldx;
-    var newDy = dy + oldy;
-    console.log([newDx, newDy]);
-    this.transform("t"+newDx+","+newDy);
-};
-var up = function(){
-};
-
-function main(){
-    
+var app = angular.module("cache", []);
+app.controller("drawController", function ($scope) {
     var snap = Snap("#canvas");
-    
-    $(".figure").on("click", function(){
-        var figure = snap.rect(50,50,50, 50);
-        figure.drag(move,start,up);
-        figure.attr({
-            fill: "white",
-            stroke: "black",
-            strokeWidth: 2
+
+    var figures = [];
+    var focused = undefined;
+    $scope.initEntity = function () {
+        var rect = snap.rect(50, 50, 100, 50);
+        var text = snap.text(77, 77, "Empty");
+        figure = snap.group(rect, text);
+        figure.drag(move, start, up);
+        rect.attr(style);
+        text.attr({
+            'text-anchor': 'start'
         });
+        text.dblclick(processText);
         figures.push(figure);
-    });
-}
+    };
+    $scope.initAttribute = function () {
+        var rect = snap.ellipse(50, 50, 50, 25);
+        var text = snap.text(50, 50, "Empty");
+        figure = snap.group(rect, text);
+        figure.drag(move, start, up);
+        rect.attr(style);
+        text.attr({
+            'text-anchor': 'start'
+        });
+        text.dblclick(processText);
+        figures.push(figure);
+    };
+
+    $scope.dialogValue = undefined;
+    $scope.dialogShow = false;
+    $scope.openDialog = function (oldValue) {
+        $scope.dialogValue = focused.attr("text");
+        $scope.$apply(function () {
+            $scope.dialogShow = true;
+        });
+    };
+    $scope.submitChange = function () {
+        $scope.dialogShow = false;
+        focused.attr("text", this.dialogValue);
+    };
+    var style = {
+        fill: "white",
+        stroke: "black",
+        strokeWidth: 2
+    };
+    var focusStyle = {
+        strokeWidth: 4
+    };
+    var unfocusedStyle = {
+        strokeWidth: 2
+    };
+
+
+    var start = function () {
+        focus(this);
+        this.data("oldt", this.transform().string);
+    };
+    var move = function (dx, dy) {
+        var values = parseCoord(this.data("oldt"));
+        oldx = parseInt(values[0]);
+        oldy = parseInt(values[1]);
+        var newDx = dx + oldx;
+        var newDy = dy + oldy;
+        this.transform("t" + newDx + "," + newDy);
+    };
+    var up = function () {};
+    var focus = function (element) {
+        if (focused) {
+            focused.attr(unfocusedStyle);
+        }
+        element.attr(focusStyle);
+        focused = element;
+    }
+
+    var processText = function () {
+        focus(this);
+        $scope.openDialog();
+        console.log("aa");
+    };
+
+    // <- [x, y]
+    var parseCoord = function (transform) {
+        var values = transform.split(/[t,]/).filter(function (el, index, arr) {
+            return el ? true : false;
+        });
+        values[0] = parseInt(values[0] || 0);
+        values[1] = parseInt(values[1] || 0);
+        return values;
+    };
+
+
+
+});
