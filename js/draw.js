e2d35d8c1742d251318529ecaf2c7dfebeeea2a4
Maxim <caliber1997@gmail.com>
2015-03-30 23:03:35 +0300

    new lib

--- js/draw.js
+++ js/draw.js
@@ -1,32 +1,30 @@
 var app = angular.module("cache", []);
 app.controller("drawController", function ($scope) {
-    var snap = Snap("#canvas");
-
+    var graph = new joint.dia.Graph;
+    var paper = new joint.dia.Paper({
+        el: $('#canvas'),
+        gridSize: 10,
+        model: graph
+    });
     var figures = [];
     var focused = undefined;
+
     $scope.initEntity = function () {
-        var rect = snap.rect(50, 50, 100, 50);
-        var text = snap.text(77, 77, "Empty");
-        figure = snap.group(rect, text);
-        figure.drag(move, start, up);
-        rect.attr(style);
-        text.attr({
-            'text-anchor': 'start'
-        });
-        text.dblclick(processText);
-        figures.push(figure);
+        this.initAttribute();
     };
     $scope.initAttribute = function () {
-        var rect = snap.ellipse(50, 50, 50, 25);
-        var text = snap.text(50, 50, "Empty");
-        figure = snap.group(rect, text);
-        figure.drag(move, start, up);
-        rect.attr(style);
-        text.attr({
-            'text-anchor': 'start'
-        });
-        text.dblclick(processText);
-        figures.push(figure);
+        var rect = new joint.shapes.basic.Rect({
+            position: {
+                x: 50,
+                y: 70
+            },
+            size: {
+                width: 100,
+                height: 40
+            }
+        })
+        graph.addCell(rect);
+        figures.push(rect);
     };
 
     $scope.dialogValue = undefined;
@@ -42,10 +40,6 @@ app.controller("drawController", function ($scope) {
         focused.attr("text", this.dialogValue);
     };
 
-
-
-
-
     //То самое удаление
     $scope.deleteObj = function () {
         $scope.dialogShow = false;
@@ -56,59 +50,4 @@ app.controller("drawController", function ($scope) {
         var ind = figures.indexOf(focused);
         figures.splice(ind);
     }
-
-
-
-
-    var style = {
-        fill: "white",
-        stroke: "black",
-        strokeWidth: 2
-    };
-    var focusStyle = {
-        strokeWidth: 4
-    };
-    var unfocusedStyle = {
-        strokeWidth: 2
-    };
-
-    var start = function () {
-        focus(this);
-        this.data("oldt", this.transform().string);
-    };
-    var move = function (dx, dy) {
-        var values = parseCoord(this.data("oldt"));
-        oldx = parseInt(values[0]);
-        oldy = parseInt(values[1]);
-        var newDx = dx + oldx;
-        var newDy = dy + oldy;
-        this.transform("t" + newDx + "," + newDy);
-    };
-    var up = function () {};
-    var focus = function (element) {
-        if (focused) {
-            focused.attr(unfocusedStyle);
-        }
-        element.attr(focusStyle);
-        focused = element;
-    }
-
-    var processText = function () {
-        focus(this);
-        $scope.openDialog();
-        console.log("aa");
-    };
-
-    // <- [x, y]
-    var parseCoord = function (transform) {
-        var values = transform.split(/[t,]/).filter(function (el, index, arr) {
-            return el ? true : false;
-        });
-        values[0] = parseInt(values[0] || 0);
-        values[1] = parseInt(values[1] || 0);
-        return values;
-    };
-
-
-
 });
