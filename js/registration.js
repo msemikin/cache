d165c3d49232320e7ab95aa3db455a8b4f365c9d
didevgen <didevgen@gmail.com>
2015-04-29 20:44:26 +0300

    Работающая с моим серваком регистрация

--- js/registration.js
+++ js/registration.js
@@ -18,3 +18,48 @@
                 //Если элемент с id-шником element_id не найден, то выводим сообщение
                 else alert("Элемент с id: " + element_id + " не найден!");
             }
+
+var cacheApp = angular.module("cache", []);
+
+cacheApp.controller("LoginController", ["$scope", "$http", function ($scope, $http) {
+
+	$scope.student = true;
+
+	$scope.teacher = false;
+
+	$scope.login = 'admin';
+
+	$scope.password = 'admin';
+
+	$scope.setTeacher = function () {
+		this.student = false;
+		this.teacher = true;
+		console.log(this);
+	};
+
+	$scope.setStudent = function () {
+		this.student = true;
+		this.teacher = false;
+	};
+
+	$scope.submitLogin = function () {
+		var serverURL = "http://localhost:57772/csp/rest/json/accounts";
+
+		// указываем класс process для div-а сообщений и плавно показываем его
+		$("#login_result").removeClass().addClass('process').text('Проверка....').fadeIn(1000);
+
+		var url = serverURL + '/' + this.login + '/' + this.password;
+
+		var responsePromise = $http.get(url);
+
+		responsePromise.error(function () {
+			window.alert('error');
+			console.log(arguments);
+		});
+
+		responsePromise.success(function (data) {
+			window.alert(data.children[0].Id);
+			window.location = "http://localhost:57772/csp/user/git/pg/workenv.html";
+		});
+	};
+}]);
