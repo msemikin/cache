e0aa9ebc3f022425534428615275a175b3387e84
irusik020596 <irusik020596@gmail.com>
2015-05-28 12:02:15 +0300

    добавление.удаление в панелях с айдишниками

--- js/projects.js
+++ js/projects.js
@@ -44,18 +44,57 @@ function edit(){
   alert("hbjhb");
 });*/
 
-
+$('#addTeacher').click( function()
+            {
+                debugger;
+                addTeacher("teacher", "2");
+            });
 function addTeacher(value, id){
-    $("#mainTeacherDiv").append('<div id = teacher'+ id +' style="padding:0;" class="panel-body"><div class = \'gorizont\'><input type="button" style = "border-style: none; width: 97%; margin: 0;padding-right: 90%;"class="btn btn-default buttonTeacher" value='+value+'> <button id="deleteTeacher" style =\'border: none;\' type="button" class="btn btn-default btn-small" onclick="delTeacher();"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div>');
+    debugger;
+    var newId = 'teacher_'+ id;
+    var newButId = 'teacherBut_'+ id;
+    $("#mainTeacherDiv").append('<div id = '+ newId +' style="padding:0;" class="panel-body"><div class = \'gorizont\'><input type="button" style = "border-style: none; width: 97%; margin: 0;padding-right: 90%;"class="btn btn-default" value='+value+'> <button id=' + newButId + ' style =\'border: none;\' type="button" class="btn btn-default btn-small" onclick="delTeacher(this.id);"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div>');
 }
 
 function delTeacher(id){
-    $("#"+ id).remove();
+    debugger;
+    var elId = 'teacher_' + id.slice(-1);
+    $('#' + elId).remove();
 }
+$("#addProject").click( function()
+           {
+             addProject("cache", "2");
+           }
+        );
 function addProject(value, id){
-    $("#mainProjectDiv").append('<div id = project'+ id + ' style="padding:0;" class="panel-body"><div class = \'gorizont\'><input type="button" style = "border-style: none;width: 90%; margin: 0;padding-right: 80%;"class="btn btn-default" value='+value+'> <button id="deleteProject" style =\'border: none;\' type="button" class="btn btn-default btn-small" onclick="delProject();"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div>');
+    var newId = 'project_'+ id;
+    var newButId = 'projectBut_'+ id;
+    $("#mainProjectDiv").append('<div id = '+ newId + ' style="padding:0;" class="panel-body"><div class = \'gorizont\'><input type="button" style = "border-style: none;width: 90%; margin: 0;padding-right: 80%;" class="btn btn-default" value='+value+'> <button id = ' + newButId + ' style =\'border: none;\' type="button" class="btn btn-default btn-small" onclick="delProject(this.id);"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></div>');
 }
 
 function delProject(id){
-    $("#"+ id).remove();
+    var elId = 'project_' + id.slice(-1);
+    $('#' + elId).remove();
 }
+
+$(document).ready(function(){
+	     	 $("#group").keypress(function(e){
+	     	   if(e.keyCode==13){
+	     	     document.getElementById('group').contentEditable = false;
+	     	   }
+	     	 });
+	     });
+$(document).ready(function(){
+	     	 $("#job").keypress(function(e){
+	     	   if(e.keyCode==13){
+	     	     document.getElementById('job').contentEditable = false;
+	     	   }
+	     	 });
+	     });
+$(document).ready(function(){
+	     	 $("#work").keypress(function(e){
+	     	   if(e.keyCode==13){
+	     	     document.getElementById('work').contentEditable = false;
+	     	   }
+	     	 });
+	     });
