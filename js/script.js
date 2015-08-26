c53bf0a4054dde7fab83a36a9824a5c4ef71dfe9
max.selekh@gmail.com <max.selekh@gmail.com>
2015-08-26 09:28:54 +0300

    пока так

--- js/script.js
+++ js/script.js
@@ -1,22 +1,89 @@
 /*FANTOM DATA*/
 var app = angular.module("db");
 app.controller("guicontroller", ["$scope", function ($scope) {
-        $scope.objects = [
-			{AttrCount: 111, Name: "Hockey puck"},
-			{AttrCount: 22, Name: "Golf club"},
-			{AttrCount: 31, Name: "Baseball bat"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"},
-			{AttrCount: 4, Name: "Lacrosse stick"}
+    $scope.user =
+        {
+            FullName: "Анатолий Иваныч"
+        };
+    $scope.objects = [
+        {
+            AttrCount: 111,
+            Name: "Hockey puck"
+        },
+        {
+            AttrCount: 22,
+            Name: "Golf club"
+        },
+        {
+            AttrCount: 31,
+            Name: "Baseball bat"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        },
+        {
+            AttrCount: 4,
+            Name: "Lacrosse stick"
+        }
 		];
-    }]);
+}]);
+
+
+(function ($) {
+    $(document).ready(function () {
+        $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+        $('#cssmenu #menu-button').on('click', function () {
+            var menu = $(this).next('ul');
+            if (menu.hasClass('open')) {
+                menu.removeClass('open');
+            } else {
+                menu.addClass('open');
+            }
+        });
+    });
+})(jQuery);
