7c5066e53708d020ed2c3e77093698cf8f5fa232
Maxim <caliber1997@gmail.com>
2016-03-13 17:01:01 +0200

    Updated db script

--- DreamTeamProj/SQL/dbproj.sql
+++ DreamTeamProj/SQL/dbproj.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Фев 29 2016 г., 19:43
+-- Время создания: Мар 12 2016 г., 16:13
 -- Версия сервера: 5.6.28-0ubuntu0.15.10.1
 -- Версия PHP: 5.6.11-1ubuntu3.1
 
@@ -83,8 +83,7 @@ CREATE TABLE IF NOT EXISTS `attrconstraint` (
 CREATE TABLE IF NOT EXISTS `attribute` (
   `attr_id` int(11) NOT NULL,
   `element_id` int(11) DEFAULT NULL,
-  `name` longtext COLLATE utf8_general_mysql500_ci,
-  `project_id` int(11) NOT NULL
+  `name` longtext COLLATE utf8_general_mysql500_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
@@ -130,8 +129,8 @@ CREATE TABLE IF NOT EXISTS `document` (
 -- Структура таблицы `entity`
 --
 
-CREATE TABLE IF NOT EXISTS `element` (
-  `element_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `entity` (
+  `entity_id` int(11) NOT NULL,
   `project_id` int(11) DEFAULT NULL,
   `name` longtext COLLATE utf8_general_mysql500_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
@@ -145,14 +144,7 @@ CREATE TABLE IF NOT EXISTS `element` (
 CREATE TABLE IF NOT EXISTS `group` (
   `group_Id` int(11) NOT NULL,
   `Name` longtext COLLATE utf8_general_mysql500_ci
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
-
---
--- Дамп данных таблицы `group`
---
-
-INSERT INTO `group` (`group_Id`, `Name`) VALUES
-(3, 'ПИ-13-1');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
@@ -222,14 +214,7 @@ CREATE TABLE IF NOT EXISTS `project` (
   `is_sent` smallint(6) DEFAULT NULL,
   `description` varchar(255) COLLATE utf8_general_mysql500_ci NOT NULL,
   `edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
-
---
--- Дамп данных таблицы `project`
---
-
-INSERT INTO `project` (`project_id`, `owner_id`, `title`, `is_ready`, `is_sent`, `description`, `edited`) VALUES
-(2, 12, 'First 1', 0, 0, 'Some more description', '2016-02-27 11:09:41');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
@@ -303,17 +288,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` longtext COLLATE utf8_general_mysql500_ci,
   `fullname` longtext COLLATE utf8_general_mysql500_ci,
   `abbr` longtext COLLATE utf8_general_mysql500_ci
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
-
---
--- Дамп данных таблицы `user`
---
-
-INSERT INTO `user` (`user_id`, `group_Id`, `email`, `password`, `fullname`, `abbr`) VALUES
-(12, NULL, 'max.semikin@gmail.com', '$2a$10$439HaY6gXBVGot5eLJ..LuMx6GSRGili1ckRPKXU6gC6VBFGo7JqK', 'max', NULL),
-(13, NULL, 'maximm@gmail.com', '$2a$10$JDqGRMUWNXKPf1dmSApiU.tDRtbE4iqRHgLcQgBW0dArSPdtSFRFm', 'Maxim Semikin', NULL),
-(14, NULL, 'maximus.semikin@gmail.com', '$2a$10$I.jb7uAzGIbCTK10nQBV0OFsE8lIWcI7B66xPf7lVFMqJWcdHBUZ2', 'jfsdkjfk', NULL),
-(15, NULL, 'max.semikin@gmaild.com', '$2a$10$OcJe9wkH41fRecsZ821GiOhbtvPmDWmAvQqPJ2XY3ERjjHoZIwqua', 'max', NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
@@ -359,8 +334,7 @@ ALTER TABLE `attrconstraint`
 --
 ALTER TABLE `attribute`
   ADD PRIMARY KEY (`attr_id`),
-  ADD KEY `FK_ATTRIBUT_REFERENCE_OBJECT` (`element_id`),
-  ADD KEY `project_id` (`project_id`);
+  ADD KEY `FK_ATTRIBUT_REFERENCE_OBJECT` (`element_id`);
 
 --
 -- Индексы таблицы `depstosourfield`
@@ -387,7 +361,7 @@ ALTER TABLE `document`
 -- Индексы таблицы `entity`
 --
 ALTER TABLE `entity`
-  ADD PRIMARY KEY (`element_id`),
+  ADD PRIMARY KEY (`entity_id`),
   ADD KEY `FK_OBJECT_REFERENCE_PROJECT` (`project_id`);
 
 --
@@ -514,12 +488,12 @@ ALTER TABLE `document`
 -- AUTO_INCREMENT для таблицы `entity`
 --
 ALTER TABLE `entity`
-  MODIFY `element_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `entity_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `group`
 --
 ALTER TABLE `group`
-  MODIFY `group_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `group_Id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `inforeq`
 --
@@ -539,7 +513,7 @@ ALTER TABLE `linkconstraint`
 -- AUTO_INCREMENT для таблицы `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `report`
 --
@@ -559,7 +533,7 @@ ALTER TABLE `statistic`
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `user_role`
 --
@@ -593,8 +567,8 @@ ALTER TABLE `attrconstraint`
 -- Ограничения внешнего ключа таблицы `attribute`
 --
 ALTER TABLE `attribute`
-  ADD CONSTRAINT `FK_ATTRIBUT_REFERENCE_OBJECT` FOREIGN KEY (`element_id`) REFERENCES `entity` (`element_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `attribute_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
+  ADD CONSTRAINT `FK_ATTRIBUTE_REFERENCE_ENTITY` FOREIGN KEY (`element_id`) REFERENCES `entity` (`entity_id`),
+  ADD CONSTRAINT `FK_ATTRIBUT_REFERENCE_OBJECT` FOREIGN KEY (`element_id`) REFERENCES `entity` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `depstosourfield`
@@ -619,7 +593,7 @@ ALTER TABLE `document`
 -- Ограничения внешнего ключа таблицы `entity`
 --
 ALTER TABLE `entity`
-  ADD CONSTRAINT `element_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
+  ADD CONSTRAINT `entity_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
 
 --
 -- Ограничения внешнего ключа таблицы `inforeq`
@@ -631,16 +605,16 @@ ALTER TABLE `inforeq`
 -- Ограничения внешнего ключа таблицы `link`
 --
 ALTER TABLE `link`
-  ADD CONSTRAINT `dsfsdfgdfdf` FOREIGN KEY (`first_el_id`) REFERENCES `entity` (`element_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `dsfsdfsdfvcx` FOREIGN KEY (`second_el_id`) REFERENCES `element` (`element_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `dsfsdfgdfdf` FOREIGN KEY (`first_el_id`) REFERENCES `entity` (`entity_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `dsfsdfsdfvcx` FOREIGN KEY (`second_el_id`) REFERENCES `entity` (`entity_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `link_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
 
 --
 -- Ограничения внешнего ключа таблицы `linkconstraint`
 --
 ALTER TABLE `linkconstraint`
-  ADD CONSTRAINT `dsfsdfs` FOREIGN KEY (`first_element`) REFERENCES `entity` (`element_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `dsfsdfsds` FOREIGN KEY (`second_element`) REFERENCES `element` (`element_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `dsfsdfs` FOREIGN KEY (`first_element`) REFERENCES `entity` (`entity_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `dsfsdfsds` FOREIGN KEY (`second_element`) REFERENCES `entity` (`entity_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `linkconstraint_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
 
 --
